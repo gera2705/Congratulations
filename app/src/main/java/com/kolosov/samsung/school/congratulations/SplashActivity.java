1d4078d7ab38a39346aadefff071c011e33f84ed
gera275 <kolosov.slava.70@gmail.com>
2021-05-04 20:50:22 +0800

    add Splash

---
+++ app/src/main/java/com/kolosov/samsung/school/congratulations/SplashActivity.java
@@ -0,0 +1,27 @@
+package com.kolosov.samsung.school.congratulations;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = new Intent(this, HomeActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    //    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//
+//        Intent intent = new Intent(this, MainActivity.class);
+//        startActivity(intent);
+//        finish();
+//    }
+}
