a6955dd7471967252c954a9ebe5af0cda1428413
gera275 <kolosov.slava.70@gmail.com>
2021-06-09 22:18:51 +0800

    Bags fix

--- app/src/main/java/com/kolosov/samsung/school/congratulations/Activities/SearchResultActivity.java
+++ app/src/main/java/com/kolosov/samsung/school/congratulations/Activities/SearchResultActivity.java
@@ -32,45 +32,67 @@ public class SearchResultActivity extends AppCompatActivity {
     private BottomNavigationView bottomNavigationView;
 
     private TextView countTextView;
-    private Button favoriteButton;
 
     private static int currentCongratulationNumber;
+    private static final String congratulationNotFound = "К сожелению поздравления на этот праздник еще не добавленны :(";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_result);
 
+        init();
+
+        loadMenu();
+
+    }
+
+    private void init(){
         //initialization
         congratulation = findViewById(R.id.congratulation_text);
         TextView smallCounterTextView = findViewById(R.id.small_count_text_view);
         bottomNavigationView = (BottomNavigationView)findViewById(R.id.navigation);
         Button copyButton = findViewById(R.id.button_copy);
         Button shareButton = findViewById(R.id.button_share);
-        favoriteButton = findViewById(R.id.favorite_button);
+        Button favoriteButton = findViewById(R.id.favorite_button);
         Button forwardArrowImageButton = findViewById(R.id.forward_arrow_image_button);
         Button backArrowImageButton = findViewById(R.id.back_arrow_image_button);
         countTextView = findViewById(R.id.result_counter);
         ImageButton backImageButton = findViewById(R.id.back_button);
         currentCongratulationNumber = 1;
+        Bundle arguments = getIntent().getExtras();
+        int flag = Integer.parseInt(arguments.get("flag").toString());
+        CongratulationDataBase db = CongratulationDataBase.getDbInstance(this.getApplicationContext());
         //initialization
 
-        Bundle arguments = getIntent().getExtras();
+        //click event
 
-        int flag =  Integer.parseInt(arguments.get("flag").toString());
+        copyButton.setOnClickListener(v -> {
+            ClipboardManager clipboard = (ClipboardManager)getSystemService(Context.CLIPBOARD_SERVICE);
+            ClipData clip = ClipData.newPlainText("your_text_to_be_copied" , congratulation.getText().toString());
+            clipboard.setPrimaryClip(clip);
+
+            Toast.makeText(SearchResultActivity.this ,"Поздравление скопировано",Toast.LENGTH_SHORT).show();
+        });
+
+        shareButton.setOnClickListener(v -> {
+            Intent sharingIntent = new Intent(Intent.ACTION_SEND);
+            sharingIntent.setType("text/plain");
+            sharingIntent.putExtra(Intent.EXTRA_TEXT, congratulation.getText().toString());
+            sharingIntent.putExtra(Intent.EXTRA_SUBJECT, "Subject");
+            startActivity(Intent.createChooser(sharingIntent, "Поделиться поздравлением"));
+        });
+
+
+        backImageButton.setOnClickListener(v -> SearchResultActivity.this.finish());
 
         if(flag == 1){
             String holidayName = arguments.get("holidayName").toString();
             String humanName = arguments.get("humanName").toString();
 
-            CongratulationDataBase db = CongratulationDataBase.getDbInstance(this.getApplicationContext());
-
             Congratulation receivedCongratulation = db.congratulationDao().getCongratulation(holidayName);
 
             String[] strings = receivedCongratulation.congratulationText.split("\\|");
-
-
-
 
             smallCounterTextView.setText("Найдено: " + strings.length);
 
@@ -78,21 +100,6 @@ public class SearchResultActivity extends AppCompatActivity {
 
             countTextView.setText( currentCongratulationNumber + "/" + strings.length);
             congratulation.setMovementMethod(new ScrollingMovementMethod());
-
-            favoriteButton.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    if(FavoriteActivity.getFavorites() == null) {
-                        FavoriteActivity.setFavorites(new ArrayList<>());
-                    }
-                    FavoriteActivity.getFavorites().add(congratulation.getText().toString());
-                    save();
-
-                    Toast.makeText(SearchResultActivity.this ,"Поздравление добавлено в избранное!",Toast.LENGTH_SHORT).show();
-
-                }
-            });
-
 
             forwardArrowImageButton.setOnClickListener(v -> {
 
@@ -116,44 +123,30 @@ public class SearchResultActivity extends AppCompatActivity {
 
             });
 
+            favoriteButton.setOnClickListener(v -> {
+                if(FavoriteActivity.getFavorites() == null) {
+                    FavoriteActivity.setFavorites(new ArrayList<>());
+                }
+
+                if(strings[currentCongratulationNumber - 1].equals(congratulationNotFound)){
+                    Toast.makeText(SearchResultActivity.this ,"Нельзя добавить то, чего нет :)",Toast.LENGTH_SHORT).show();
+                }else {
+                    FavoriteActivity.getFavorites().add(strings[currentCongratulationNumber - 1]);
+                    save();
+
+                    Toast.makeText(SearchResultActivity.this ,"Поздравление добавлено в избранное!",Toast.LENGTH_SHORT).show();
+                }
+
+
+            });
+
         }else if(flag == 2){
             String text = arguments.get("text").toString();
-
+            countTextView.setText( 1 + "/" + 1);
+            favoriteButton.setEnabled(false);
             congratulation.setText(text);
         }
-
-
-
-
-
-        loadMenu();
-
-
-
-        backImageButton.setOnClickListener(v -> SearchResultActivity.this.finish());
-
-
-        copyButton.setOnClickListener(v -> {
-            ClipboardManager clipboard = (ClipboardManager)getSystemService(Context.CLIPBOARD_SERVICE);
-            ClipData clip = ClipData.newPlainText("your_text_to_be_copied" , congratulation.getText().toString());
-            clipboard.setPrimaryClip(clip);
-
-            Toast.makeText(SearchResultActivity.this ,"Поздравление скопировано",Toast.LENGTH_SHORT).show();
-        });
-
-
-
-        shareButton.setOnClickListener(v -> {
-
-            Intent sharingIntent = new Intent(Intent.ACTION_SEND);
-            sharingIntent.setType("text/plain");
-            sharingIntent.putExtra(Intent.EXTRA_TEXT, congratulation.getText().toString());
-            sharingIntent.putExtra(Intent.EXTRA_SUBJECT, "Subject");
-            startActivity(Intent.createChooser(sharingIntent, "Поделиться поздравлением"));
-
-        });
-
-
+        //click event
     }
 
     private void save() {
@@ -172,36 +165,32 @@ public class SearchResultActivity extends AppCompatActivity {
         MenuItem menuItem = menu.getItem(1);
         menuItem.setChecked(true);
 
-        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                switch (item.getItemId()){
+        bottomNavigationView.setOnNavigationItemSelectedListener(item -> {
+            switch (item.getItemId()){
+
+                case R.id.home:
+                    Intent intent = new Intent(SearchResultActivity.this , HomeActivity.class);
+                    startActivity(intent);
+                    break;
 
 
-                    case R.id.home:
-                        Intent intent = new Intent(SearchResultActivity.this , HomeActivity.class);
-                        startActivity(intent);
-                        break;
+                case R.id.calendar:
+                    Intent intent1 = new Intent(SearchResultActivity.this , CalendarActivity.class);
+                    startActivity(intent1);
+                    break;
+                case R.id.favorite:
+                    Intent intent2 = new Intent(SearchResultActivity.this , FavoriteActivity.class);
+                    startActivity(intent2);
+                    break;
 
+                case R.id.search:
+                    Intent intent4 = new Intent(SearchResultActivity.this , SearchActivity.class);
+                    startActivity(intent4);
+                    break;
 
-                    case R.id.calendar:
-                        Intent intent1 = new Intent(SearchResultActivity.this , CalendarActivity.class);
-                        startActivity(intent1);
-                        break;
-                    case R.id.favorite:
-                        Intent intent2 = new Intent(SearchResultActivity.this , FavoriteActivity.class);
-                        startActivity(intent2);
-                        break;
-
-                    case R.id.search:
-                        Intent intent4 = new Intent(SearchResultActivity.this , SearchActivity.class);
-                        startActivity(intent4);
-                        break;
-
-                }
-
-                return false;
             }
+
+            return false;
         });
     }
 }
